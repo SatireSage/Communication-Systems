2304cf143526148eb7f88f85a006eb9890093c47
sahajsingh <ssa365@sfu.ca>
2023-02-18 02:47:50 -0800

    setup bbuff

--- CMPT_300/As2/bbuff.c
+++ CMPT_300/As2/bbuff.c
@@ -1,21 +1,45 @@
 #include "bbuff.h"
+#include <stddef.h>
+#include <pthread.h>
+
+static void *buffer[BUFFER_SIZE];
+static int position = 0;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void bbuff_init(void)
 {
-    // TODO
+    // initialize buffer
+    position = 0;
+    pthread_mutex_init(&mutex, NULL);
 }
 
 void bbuff_blocking_insert(void *item)
 {
-    // TODO
+    // insert item into buffer
+    mutex_lock(&mutex);
+    buffer[position] = item;
+    position = (position + 1) % BUFFER_SIZE;
+    mutex_unlock(&mutex);
 }
 
 void *bbuff_blocking_extract(void)
 {
-    // TODO
+    // check if buffer is empty
+    if (bbuff_is_empty())
+        return NULL;
+    // extract item from buffer
+    mutex_lock(&mutex);
+    void *item = buffer[position];
+    buffer[position] = NULL;
+    position--; // MIGHT USE:   position = (position - 1) % BUFFER_SIZE;
+    mutex_unlock(&mutex);
+    return item;
 }
 
-_Bool bbuff_is_empty(void)
+bool bbuff_is_empty(void)
 {
-    // TODO
+    if (position == 0)
+        return true;
+    else
+        return false;
 }
--- CMPT_300/As2/bbuff.h
+++ CMPT_300/As2/bbuff.h
@@ -1,16 +1,19 @@
 #ifndef BBUFF_H
 #define BBUFF_H
 
+#include <stdbool.h>
+
 // Source for _Bool definition: https://e2e.ti.com/support/tools/code-composer-studio-group/ccs/f/code-composer-studio-forum/438557/_bool-is-undefined
-#if defined(__cplusplus)
-typedef bool _Bool;
-#endif
+// #if defined(__cplusplus)
+// typedef bool _Bool;
+// #endif
 
 #define BUFFER_SIZE 10
 
 void bbuff_init(void);
 void bbuff_blocking_insert(void *item);
 void *bbuff_blocking_extract(void);
-_Bool bbuff_is_empty(void);
+// _Bool bbuff_is_empty(void);
+bool bbuff_is_empty(void);
 
 #endif
